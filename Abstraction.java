d031b5020073faf4e28dc41f095081f1a66d6bb0
Komal0patel <155667156+Komal0patel@users.noreply.github.com>
2025-06-18 14:26:16 +0530

    Update and rename abstraction.java to Abstraction.java

--- Abstraction.java
+++ Abstraction.java
@@ -9,7 +9,7 @@ class B extends A {// Class B extends abstract class A and provides implementati
         System.out.println("Abstraction performed");
     }
 }
-public class Main {// Main class with the main method to execute the program
+public class Abstraction {// Main class with the main method to execute the program
     public static void main(String[] args) {
         // Creating a reference of abstract class A and assigning it to an object of subclass B
         // This is an example of dynamic method dispatch (runtime polymorphism)
