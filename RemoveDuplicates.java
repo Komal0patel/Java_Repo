97274d587a6099f22d9d3770b466b9e4a770207e
Komal0patel <155667156+Komal0patel@users.noreply.github.com>
2025-07-02 13:20:03 +0530

    Create RemoveDuplicates.java

---
+++ RemoveDuplicates.java
@@ -0,0 +1,33 @@
+ class Solution {
+
+    // Method to remove duplicates in-place from a sorted array
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+        Solution sol = new Solution();
+        int k = sol.removeDuplicates(nums);
+
+        System.out.println("Number of unique elements: " + k);
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
